16ab2f8d62cd44f9f2d076535a9b378e8981a0de
Connor <connor.cs@protonmail.com>
2022-12-08 11:04:39 -0500

    setup react-router

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <div>
+        <button>Browse TV</button>
+        <button>Browse Movies</button>
+    </div>
+  )
+}
