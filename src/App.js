965476c22756fb1f8139ec8e514640c11117c71c
Connor <connor.cs@protonmail.com>
2022-12-08 11:31:39 -0500

    add basic links

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
 import LandingPage from './Components/LandingPage';
+import TVShows from './Components/TVShows';
+import Movies from './Components/Movies';
+import { Route, Routes } from 'react-router-dom';
+import About from './Components/About';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <LandingPage />
+        <Route exact path='/about' element={<About />} ></Route>
+        <Route exact path='/' element={<LandingPage />} ></Route>
+        <Route exact path='/shows' element={<TVShows />} ></Route>
+        <Route exact path='/movies' element={<Movies />} ></Route>
       </Routes>
     </div>
   );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
-  return (
-    <div>
-    <h1>NavBar</h1>
-    </div>
-  )
+    return (
+        <div>
+            <NavLink to='/about'>About</NavLink>
+            <NavLink to='/'><h1>NavBar</h1></NavLink>
+            <NavLink to='/movies'>Movies</NavLink>
+            <NavLink to='/shows'>Shows</NavLink>
+        </div>
+    )
 }
