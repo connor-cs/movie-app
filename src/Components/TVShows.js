c7d8421256c52493afb013f10d198bd6d8ab5f63
Connor <connor.cs@protonmail.com>
2022-12-25 23:48:45 -0500

    tv shows

--- src/Components/TVShows.js
+++ src/Components/TVShows.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 import NavBar from './NavBar'
 
 export default function TVShows() {
+
+  const [shows, setShows] = useState([])
+  const {tvSearch, setTvSearch} = useState()
+  
   return (
     <div>
-        Popular TV shows
+        <form>
+          <input type="text" value={tvSearch}></input>
+          <button type="submit">search</button>
+        </form>
+        <div className='show-container'>
+
+        </div>
     </div>
   )
 }
