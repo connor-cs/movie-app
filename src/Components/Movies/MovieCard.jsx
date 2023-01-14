1efc5d8a805208f32d57d851c7f43a4a9c29e842
Connor <connor.cs@protonmail.com>
2023-01-14 15:30:47 -0500

    tv shows rendering

--- src/Components/Movies/MovieCard.jsx
+++ src/Components/Movies/MovieCard.jsx
@@ -10,9 +10,12 @@ export default function MovieCard({ movie }) {
     <div className='card' key={movie.id}>
       <h3>{movie.title}</h3>
       <p>Released: {movie.release_date}</p>
-      <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} />
+      {movie.poster_path ?
+        <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={`${movie.title} poster`} /> :
+        <div className='filler-poster'></div>}
+
       <p>Average score: {movie.vote_average}</p>
-      <p>Add to list <AiOutlineStar className='star-icon' onClick={handleLikeClick}/></p>
+      <p>Add to list <AiOutlineStar className='star-icon' onClick={handleLikeClick} /></p>
     </div>
   )
 }
