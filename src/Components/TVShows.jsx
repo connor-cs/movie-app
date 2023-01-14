ebb5ac3c0ce121828dcb6605a813a1f7884187ab
Connor <connor.cs@protonmail.com>
2023-01-13 20:08:39 -0500

    tv show fetch

--- src/Components/TVShows.jsx
+++ src/Components/TVShows.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
-import NavBar from './NavBar'
+import { json } from 'react-router-dom'
 
 export default function TVShows() {
 
   const [shows, setShows] = useState([])
   const {tvSearch, setTvSearch} = useState()
+  const key = process.env.REACT_APP_API_KEY
+
+  useEffect(() => {
+    const fetchShows = async () => {
+      const showData = await fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&language=en-US&page=1`)
+      const json = await showData.json()
+      setShows(json.results)
+    }
+    fetchShows()
+  }, [])
   
   return (
     <div>
