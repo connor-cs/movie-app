08c5c4f50c42f20294c6adfb0d614d967e96d203
Connor <connor.cs@protonmail.com>
2023-01-17 12:11:30 -0500

    setting up POST to firebase

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { db } from '../firebase-config'
-import { collection, getDocs } from 'firebase/firestore'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { collection, getDocs, addDoc, updateDoc, doc } from 'firebase/firestore'
+
+
 export default function About() {
 
   const usersCollectionRef = collection(db, "users")
   const [users, setUsers] = useState([])
+  const [newUserName, setNewUserName] = useState('')
+  const [newPass, setNewPass] = useState('')
 
   useEffect(() => {
     const getUsers = async () => {
@@ -22,18 +24,43 @@ export default function About() {
     getUsers()
   }, [])
 
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, {username: newUserName, password: newPass})
+  }
+
+  const updateUserName = async (id, username) => {
+    //user doc function to select particular document from firestore
+    const userDoc = doc(db, "users", id)
+    //set updated info in obj
+    const newFields = {username: username}
+    //update doc
+    await updateDoc(userDoc,newFields)
+  }
 
   return (
     <div style={{ 'backgroundColor': "white" }}>
       
-      <h1>testtesttest</h1>
+      <h1>firebase test page</h1>
+      <br></br>
       {users.map(user => {
-        return <div>
-          <h1>{user.username}</h1>
-          <h1>{user.password} </h1>
-          <h1>{user.id}</h1>
+        return <div key={user.id}>
+          <h1>username: {user.username}</h1>
+          <h1>password: {user.password} </h1>
+          <h1>userid: {user.id}</h1>
+          <button onClick={()=> {updateUserName(user.id, user.username)}}>change username</button>
+          <br></br>
           </div>
       })}
+      <div>
+        <h2>test form for creating new user in firebase:</h2>
+        <input 
+          placeholder='username..'
+          onChange={(e)=>setNewUserName(e.target.value)}/>
+        <input 
+          placeholder='enter password'
+          onChange={(e)=>(setNewPass(e.target.value))}/>
+        <button onClick={createUser}>Create new user</button>
+      </div>
     
     </div>
   )
