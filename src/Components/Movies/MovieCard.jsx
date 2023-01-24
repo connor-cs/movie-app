6251c6fb3e384e3ad90bbe646ac2d9e0cb2321d2
Connor <connor.cs@protonmail.com>
2023-01-24 13:33:02 -0500

    render user watchlist

--- src/Components/Movies/MovieCard.jsx
+++ src/Components/Movies/MovieCard.jsx
@@ -23,11 +23,11 @@ export default function MovieCard({ movie, handleClick }) {
   return (
     <div className='card' key={movie.id}>
       <h3>{movie.title}</h3>
-      <p>Released: {movie.release_date}</p>
+      {movie.release_date ? <p>Released: {movie.release_date}</p> : null}
       {movie.poster_path ?
         <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={`${movie.title} poster`} /> :
         <div className='filler-poster'></div>}
-      <p>Average score: {movie.vote_average}</p>
+      {movie.vote_average ? <p>Average score: {movie.vote_average}</p> : null}
       {/* <p>{movie.overview}</p> */}
       <p>Add to list <AiOutlineStar className='star-icon' onClick={()=>handleClick(movie.id, movie.title, movie.poster_path)} /></p>
     </div>
--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,29 +1,37 @@
-import React from "react";
-
-import { useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useAuthContext, UserContext } from "./Context";
-
 import { db } from "../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import MovieCard from './Movies/MovieCard'
 
 export default function User() {
-  const { loggedInState, setLoggedInState, currentUser } =
-    useContext(UserContext);
+  const { loggedInState, currentUser } = useContext(UserContext);
   const navigate = useNavigate();
   const { logout } = useAuthContext();
   const [active, setActive] = useState(false);
+  const [watchlist, setWatchlist] = useState([]);
 
+  useEffect(() => {
+    const watchlistRef = collection(db, `users/${currentUser.uid}/watchlist`);
+    const getWatchlist = async () => {
+      const data = await getDocs(watchlistRef);
+      setWatchlist(data.docs.map(doc=>doc.data()));
+    };
+    getWatchlist();
+  }, []);
+
+  console.log('watchlist:', watchlist)
   return (
     <>
       <div className="user-page-header">
         <h1>Welcome user {currentUser.email}</h1>
-        <h2>Login state: {loggedInState}</h2>
         <button>Account actions</button>
         <button onClick={handleLogout}>Logout</button>
       </div>
-      <div className="saved-movies-container"></div>
+      <div className="saved-movies-container">
+        {watchlist ? watchlist.map(item => <MovieCard movie={item}/>) : null}
+      </div>
     </>
   );
 
