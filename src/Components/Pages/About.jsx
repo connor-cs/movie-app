22b1ce54b5f20081f019b0130ae7be6272aff448
Connor <connor.cs@protonmail.com>
2023-01-27 11:16:37 -0500

    readme

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
     <div>
       <h3>React app built with Firebase Auth and Firestore</h3>
       <ul>
-        <li>Usees Firebase Auth to authenticate and manage users</li>
+        <li>Uses Firebase Auth to authenticate and manage users</li>
         <li>Allows a user to search for movies and tv shows</li>
         <li>Add shows and movies to a watchlist</li>
         <li>Uses Firestore to save user data</li>
