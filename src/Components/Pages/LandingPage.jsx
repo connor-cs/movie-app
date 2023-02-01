988a180f9fc0bfcad1c3dc2868f11e90991f7ea6
Connor <connor.cs@protonmail.com>
2023-01-31 20:29:10 -0500

    more css

--- src/Components/Pages/LandingPage.jsx
+++ src/Components/Pages/LandingPage.jsx
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
   return (
     <div className="landing-page">
+      <h1 className="landing-header">Entertainment Hub</h1>
       <div className="button-div">
-        <button>
+        <button className="landing-page-button">
           <Link to="/shows">Browse TV</Link>
         </button>
-        <button>
+        <button className="landing-page-button">
           <Link to="/movies">Browse Movies</Link>
         </button>
       </div>
